d9dd62921d4ac9598c6119b2656f440532de26f2
Andrew Thiessen <squaids449@gmail.com>
2019-12-03 18:21:28 -0700

    fix tile movement fix tile selection fix tile tweening implement matching add tile falling add tile respawn add drag capability removed unnecessary variables

--- app.js
+++ app.js
@@ -1,102 +1,401 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    backgroundColor: '#2d2d2d',
-    parent: 'ele-match',
-    scene: {
-        preload: preload,
-        create: create
-    }
-};
-
-const game = new Phaser.Game(config)
-
+let game
 let elementSize = 64
-let elementSpace = 2
-let elementSizeSpaced = elementSize + elementSpace
-let boardCols
-let boardRows;
 let matchMin = 3
+let boardSize = 9
+let swapSpeed = 200
+let fallSpeed = 100
+let destroySpeed = 200
 
-let elements
+const HORIZONTAL = 1;
+const VERTICAL = 2;
 
-const colors = {
-    0: 'yellow',
-    1: 'red',
-    2: 'green',
-    3: 'purple',
-    4: 'blue'
-}
-
-function preload(){
-    this.load.spritesheet('elements', 'assets/tiles/elements64.png', {frameWidth: elementSize, frameHeight: elementSize})
+window.onload = function(){
+    var config = {
+        type: Phaser.AUTO,
+        width: 580,
+        height: 580,
+        backgroundColor: '#2d2d2d',
+        parent: 'ele-match',
+        scene: startGame
+    };
     
+    game = new Phaser.Game(config)
 }
 
-function create(){
-    boardRows = Math.floor(game.scale.height / elementSize)
-    boardCols = Math.floor(game.scale.width / elementSize)
-    elements = this.add.group()
-    let ids = []
-    let move = false
-    this.score = 0
-    this.active1 = null
-    this.active2 = null
+class startGame extends Phaser.Scene{
+    constructor(){
+        super('StartGame');
+    }
+    
+    preload(){
+        this.load.spritesheet('elements', 'assets/tiles/elements64.png', {
+            frameWidth: elementSize, 
+            frameHeight: elementSize
+        })
+    }
+    
+    create(){
+        this.selectable = true
+        this.drag = false
+        this.generateBoard()
+        this.activeElement = null
+        this.input.on('pointerdown', this.selectElement, this)
+        this.input.on('pointermove', this.startDrag, this)
+        this.input.on('pointerup', this.stopDrag, this)
+    }
+    
+    generateBoard(){
+        this.boardArray = []
+        this.poolArray = []
+        this.elements = this.add.group()
+        for(let i = 0; i < boardSize; i++){
+            this.boardArray[i] = []
+            for(let j = 0; j < boardSize; j++){
+                let element = this.add.sprite(elementSize * j + elementSize / 2, elementSize * i + elementSize / 2, 'elements')
+                this.elements.add(element)
+                let elemId = i + j * boardSize
+                element.name = 'element ' + i.toString() + ' x ' + j.toString();
+                element.id = elemId
+                do {
+                    let randColor = Phaser.Math.Between(0, 4)
+                    element.setFrame(randColor)
+                    this.boardArray[i][j] = {
+                        color: randColor,
+                        elementSprite: element,
+                        isEmpty: false
+                    }
+                }while(this.isMatch(i, j))
+            }
+        }
+    }
 
-    for(let i = 0; i < boardCols; i++){
-        for(let j = 0; j < boardRows; j++){
-            let element = elements.create(i, j, 'elements', Phaser.Math.Between(0, 4)).setInteractive();
-            this.input.setDraggable(element)
-            let elemId = i + j * boardCols
-            element.name = 'element ' + i.toString() + ' x ' + j.toString();
-            element.id = elemId
-            ids.push(colors[element.frame.name])
+    isMatch(row, col){
+        return this.horizontalMatch(row, col) || this.verticalMatch(row, col)
+    }
 
+    horizontalMatch(row, col){
+        return this.elementAt(row, col).color == this.elementAt(row, col - 1).color && this.elementAt(row, col).color == this.elementAt(row, col - 2).color
+    }
 
-            this.input.on('dragstart', function(pointer){
-                // console.log("Clicked", this.name, colors[this.frame.name], this.id, "Position", this.posX, this.posY)
-                move = true
-            })
+    verticalMatch(row, col){
+        return this.elementAt(row, col).color == this.elementAt(row - 1, col).color && this.elementAt(row, col).color == this.elementAt(row - 2, col).color
+    }
 
-            this.input.on('drag', function(pointer, gameObject, dragX, dragY){
-                if (move){
-                    gameObject.x = dragX
-                    gameObject.y = dragY
-                    // console.log(move, this.posX, this.posY)
+    elementAt(row, col){
+        if(row < 0 || row >= boardSize || col < 0 || col >= boardSize){
+            return -1
+        }
+        return this.boardArray[row][col]
+    }
+
+    selectElement(pointer){
+        if(this.selectable){
+            this.drag = true
+            let row = Math.floor(pointer.x / elementSize)
+            let col = Math.floor(pointer.y / elementSize)
+            let selectedElement = this.elementAt(col, row)
+            if(selectedElement != -1){
+                if(this.activeElement == null){
+                    selectedElement.elementSprite.setScale(1.2)
+                    selectedElement.elementSprite.setDepth(1)
+                    this.activeElement = selectedElement
                 }
-            })
-
-            this.input.on('dragend', function(pointer){
-                move = false
-            })
+                else{
+                    if(this.areTheSame(selectedElement, this.activeElement)){
+                        this.activeElement.elementSprite.setScale(1)
+                        this.activeElement = null
+                    }
+                    else{
+                        if(this.areNext(selectedElement, this.activeElement)){
+                            this.activeElement.elementSprite.setScale(1)
+                            this.swapElements(this.activeElement, selectedElement, true)
+                        }
+                        else {
+                            this.activeElement.elementSprite.setScale(1)
+                            selectedElement.elementSprite.setScale(1.2)
+                            this.activeElement = selectedElement
+                        }
+                    }
+                }
+            }
         }
     }
 
 
-    console.log(sectionArray(ids, 12))
-
-    Phaser.Actions.GridAlign(elements.getChildren(), {
-        width: 12,
-        height: 9,
-        cellWidth: 64,
-        cellHeight: 64,
-        x: 48,
-        y: 40
-    })
-
-}
-
-function sectionArray(array, size){
-    let index = 0
-    let tempArray = []
-    let i = 0
-    for (index = 0; index < array.length; index += size){
-        let chunk = array.slice(index, index+size)
-        tempArray.push(chunk)
-        i++
+    areTheSame(elem1, elem2){
+        return this.getElementRow(elem1) == this.getElementRow(elem2) && this.getElementCol(elem1) == this.getElementCol(elem2)
     }
-    return tempArray
+
+    getElementRow(element){
+        return Math.floor(element.elementSprite.y / elementSize)
+    }
+
+    getElementCol(element){
+        return Math.floor(element.elementSprite.x / elementSize)
+    }
+
+    areNext(elem1, elem2){
+        return Math.abs(this.getElementRow(elem1) - this.getElementRow(elem2)) + Math.abs(this.getElementCol(elem1) - this.getElementCol(elem2)) == 1
+    }
+
+    swapElements(elem1, elem2, swapBack){
+        this.swappingElements = 2
+        this.selectable = false
+        this.drag = false
+        let fromColor = elem1.color
+        let fromSprite = elem1.elementSprite
+        let toColor = elem2.color
+        let toSprite = elem2.elementSprite
+        let elem1Row = this.getElementRow(elem1)
+        let elem1Col = this.getElementCol(elem1)
+        let elem2Row = this.getElementRow(elem2)
+        let elem2Col = this.getElementCol(elem2)
+        this.boardArray[elem1Row][elem1Col].color = toColor
+        this.boardArray[elem1Row][elem1Col].elementSprite = toSprite
+        this.boardArray[elem2Row][elem2Col].color = fromColor
+        this.boardArray[elem2Row][elem2Col].elementSprite = fromSprite
+        this.tweenElement(elem1, elem2, swapBack)
+        this.tweenElement(elem2, elem1, swapBack)
+    }
+
+    tweenElement(elem1, elem2, swapBack){
+        let row = this.getElementRow(elem1)
+        let col = this.getElementCol(elem1)
+        this.tweens.add({
+            targets: this.boardArray[row][col].elementSprite,
+            x: col * elementSize + elementSize / 2,
+            y: row * elementSize + elementSize / 2,
+            duration: swapSpeed,
+            callbackScope: this,
+            onComplete: function(){
+                this.swappingElements --;
+                if(this.swappingElements == 0){
+                    if(!this.matchInBoard() && swapBack){
+                        this.swapElements(elem1, elem2, false)
+                    }
+                    else{
+                        if(this.matchInBoard()){
+                            this.handleMatches()
+                        }
+                        else {
+                            this.selectable = true
+                            this.activeElement = null
+                        }
+                    }
+                }
+            }
+        })
+    }
+
+    matchInBoard(){
+        for(let i = 0; i < boardSize; i++){
+            for(let j = 0; j < boardSize; j++){
+                if(this.isMatch(i, j)){
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    handleMatches(){
+        this.removeMap = [];
+        for(let i = 0; i < boardSize; i++){
+            this.removeMap[i] = []
+            for(let j = 0; j < boardSize; j++){
+                this.removeMap[i].push(0)
+            }
+        }
+        this.markMatches(HORIZONTAL)
+        this.markMatches(VERTICAL)
+        this.destroyElements()
+    }
+
+    markMatches(direction){
+        for(let i = 0; i < boardSize; i++){
+            let colorStreak = 1
+            let currentColor = -1
+            let startStreak = 0
+            let colorToWatch = 0
+            for(let j = 0; j < boardSize; j++){
+                if(direction == HORIZONTAL){
+                    colorToWatch = this.elementAt(i, j).color
+                }
+                else {
+                    colorToWatch = this.elementAt(j, i).color
+                }
+                if(colorToWatch == currentColor){
+                    colorStreak++
+                }
+                if(colorToWatch != currentColor || j == boardSize - 1){
+                    if(colorStreak >= 3){
+                        if(direction == HORIZONTAL){
+                            console.log("HORIZONTAL :: Length = " + colorStreak + " :: Start = (" + i + "," + startStreak + ") :: Color = ", currentColor)
+                        }
+                        else {
+                            console.log("VERTICAL :: Length = " + colorStreak + " :: Start = (" + i + "," + startStreak + ") :: Color = ", currentColor)
+                        }
+                        for(let k = 0; k < colorStreak; k++){
+                            if(direction == HORIZONTAL){
+                                this.removeMap[i][startStreak + k]++
+                            }
+                            else {
+                                this.removeMap[startStreak + k][i]++
+                            }
+                        }
+                    }
+                    startStreak = j
+                    colorStreak = 1
+                    currentColor = colorToWatch
+                }
+            }
+        }
+    }
+
+    destroyElements(){
+        let destroyed = 0
+        for(let i = 0; i < boardSize; i++){
+            for(let j = 0; j < boardSize; j++){
+                if(this.removeMap[i][j] > 0){
+                    destroyed++
+                    this.tweens.add({
+                        targets: this.boardArray[i][j].elementSprite,
+                        alpha: 0.5,
+                        duration: destroySpeed,
+                        callbackScope: this,
+                        onComplete: function(){
+                            destroyed--
+                            this.boardArray[i][j].elementSprite.visible = false
+                            this.poolArray.push(this.boardArray[i][j].elementSprite)
+                            // console.log(this.boardArray[i][j].elementSprite)
+                            if(destroyed == 0){
+                                this.makeElementsFall()
+                                this.respawnElements()
+                            }
+                        }
+                    })
+                    this.boardArray[i][j].isEmpty = true;
+                }
+            }
+        }
+    }
+
+    makeElementsFall(){
+        for(let i = boardSize - 2; i >= 0; i--){
+            for(let j = 0; j < boardSize; j++){
+                if(!this.boardArray[i][j].isEmpty){
+                    let fallTiles = this.holesBelow(i, j);
+                    if(fallTiles > 0){
+                        this.tweens.add({
+                            targets: this.boardArray[i][j].elementSprite,
+                            y: this.boardArray[i][j].elementSprite.y + fallTiles * elementSize,
+                            duration: fallSpeed * fallTiles
+                        })
+                        this.boardArray[i + fallTiles][j] = {
+                            elementSprite: this.boardArray[i][j].elementSprite,
+                            color: this.boardArray[i][j].color,
+                            isEmpty: false
+                        }
+                        this.boardArray[i][j].isEmpty = true
+                    }
+                }
+            }
+        }
+    }
+
+    holesBelow(row, col){
+        let res = 0
+        for(let i = row + 1; i < boardSize; i++){
+            if(this.boardArray[i][col].isEmpty){
+                res++
+            }
+        }
+        return res
+    }
+
+    respawnElements(){
+        let respawned = 0
+        for(let i = 0; i < boardSize; i++){
+            let emptySpots = this.holesInCol(i)
+            if(emptySpots > 0){
+                for(let j = 0; j < emptySpots; j++){
+                    respawned++
+                    let randColor = Phaser.Math.Between(0, 4)
+                    this.boardArray[j][i].color = randColor
+                    this.boardArray[j][i].elementSprite = this.poolArray.pop()
+                    this.boardArray[j][i].elementSprite.setFrame(randColor);
+                    this.boardArray[j][i].elementSprite.visible = true
+                    this.boardArray[j][i].elementSprite.x = elementSize * i + elementSize / 2
+                    this.boardArray[j][i].elementSprite.y = elementSize / 2 - (emptySpots - j) * elementSize
+                    this.boardArray[j][i].elementSprite.alpha = 1
+                    this.boardArray[j][i].isEmpty = false
+                    this.tweens.add({
+                        targets: this.boardArray[j][i].elementSprite,
+                        y: elementSize * j + elementSize / 2,
+                        duration: fallSpeed * emptySpots,
+                        callbackScope: this,
+                        onComplete: function(){
+                            respawned--
+                            if(respawned == 0){
+                                if(this.matchInBoard()){
+                                    this.time.addEvent({
+                                        delay: 250,
+                                        callback: this.handleMatches()
+                                    })
+                                }
+                                else {
+                                    this.selectable = true
+                                    this.activeElement = null
+                                }
+                            }
+                        }
+                    })
+                }
+            }
+        }
+    }
+
+    holesInCol(col){
+        let res = 0
+        for(let i = 0; i < boardSize; i++){
+            if(this.boardArray[i][col].isEmpty){
+                res++
+            }
+        }
+        return res
+    }
+
+    startDrag(pointer){
+        if(this.drag && this.activeElement != null){
+            let delX = pointer.downX - pointer.x
+            let delY = pointer.downY - pointer.y
+            let delRow = 0
+            let delCol = 0
+            if(delX > elementSize / 2 && Math.abs(delY) < elementSize / 4){
+                delCol = -1
+            }
+            if(delX < -elementSize / 2 && Math.abs(delY) < elementSize / 4){
+                delCol = 1
+            }
+            if(delY > elementSize / 2 && Math.abs(delX) < elementSize / 4){
+                delRow = -1
+            }
+            if(delY < -elementSize / 2 && Math.abs(delX) < elementSize / 4){
+                delRow = 1
+            }
+            if(delRow + delCol != 0){
+                let selected = this.elementAt(this.getElementRow(this.activeElement) + delRow, this.getElementCol(this.activeElement) + delCol)
+                if(selected != -1){
+                    this.activeElement.elementSprite.setScale(1);
+                    this.swapElements(this.activeElement, selected, true)
+                }
+            }
+        }
+    }
+
+    stopDrag(){
+        this.drag = false
+    }
 }
 
 
